142bb2c6cb1ac3cb55ec3b8c658c05e797692ab4
IgorFZ <igorf.zamp@gmail.com>
2023-02-16 17:32:40 -0300

    configuring the serialiazer for the url of the active storage images

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
     # validates :image, file_size: { less_than_or_equal_to: 5.megabytes },
     #           file_content_type: { allow: ['image/jpeg', 'image/png', 'image/gif'] }
 
-    # def image_url
-    #     Rails.application.routes.url_helpers.url_for(image) if image.attached?
-    # end
+    def image_url
+        Rails.application.routes.url_helpers.url_for(image) if image.attached?
+    end
 end
