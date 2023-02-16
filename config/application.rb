3229d7e279816604e0ef1e52fc74bf589cd0b78f
IgorFZ <igorf.zamp@gmail.com>
2023-02-16 11:33:58 -0300

    config cookies

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,16 @@ module Ecommerce
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # This also configures session_options for use below
+    config.session_store :cookie_store, key: '_interslice_session'
+
+    config.middleware.use ActionDispatch::Cookies
+
+    config.middleware.use config.session_store, config.session_options
+
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use Rack::MethodOverride
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
