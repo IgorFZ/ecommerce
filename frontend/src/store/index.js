613dfb4120f7cd15e5628c156c1aad9d34be8e49
IgorFZ <igorf.zamp@gmail.com>
2023-02-20 09:53:02 -0300

    vuex-persistedstate

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex'
+import createPersistedState from "vuex-persistedstate";
 import sessionManager from '../store/modules/session_manager'
 import postsManager from '../store/modules/posts_manager'
 import productsManager from '../store/modules/products_manager'
 import ordersManager from '../store/modules/orders_manager'
 
 const store = createStore({
+    plugins: [createPersistedState()],
     modules: {
         sessionManager,
         postsManager,
