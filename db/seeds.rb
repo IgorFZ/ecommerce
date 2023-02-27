fee7cfa2564e8007fb9121bac2f2d0c9081daaf9
IgorFZ <igorf.zamp@gmail.com>
2023-02-27 15:59:25 -0300

    adding some test data to rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Categories
+Category.create(name: "Children's")
+Category.create(name: "Women's")
+Category.create(name: "Men's")
 
+# Posts
+post = Post.create(title: "Runway-Inspired Trends", content: "Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Neque volutpat ac tincidunt vitae semper quis lectus nulla. Congue mauris rhoncus aenean vel 
+        elit scelerisque mauris. Faucibus scelerisque eleifend donec pretium. Quis lectus nulla at volutpat. 
+        Vel elit scelerisque mauris pellentesque. Nulla facilisi cras fermentum odio eu. 
+        Lacus suspendisse faucibus interdum posuere lorem. Eget aliquet nibh praesent tristique magna sit amet purus. 
+        Non nisi est sit amet facilisis magna etiam tempor. Diam quis enim lobortis scelerisque fermentum dui.")
+post.image.attach(io: File.open(Rails.root.join('frontend/src/assets/img/blog/b4.jpg')), filename: "b4.jpg")
+
+post = Post.create(title: "Must-Have Skater Girl Iten", content: "Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Neque volutpat ac tincidunt vitae semper quis lectus nulla. Congue mauris rhoncus aenean vel 
+        elit scelerisque mauris. Faucibus scelerisque eleifend donec pretium. Quis lectus nulla at volutpat. 
+        Vel elit scelerisque mauris pellentesque. Nulla facilisi cras fermentum odio eu. 
+        Lacus suspendisse faucibus interdum posuere lorem. Eget aliquet nibh praesent tristique magna sit amet purus. 
+        Non nisi est sit amet facilisis magna etiam tempor. Diam quis enim lobortis scelerisque fermentum dui.")
+post.image.attach(io: File.open(Rails.root.join('frontend/src/assets/img/blog/b3.jpg')), filename: "b3.jpg")
+
+post = Post.create(title: "How to Style a Quiff", content: "Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Neque volutpat ac tincidunt vitae semper quis lectus nulla. Congue mauris rhoncus aenean vel 
+        elit scelerisque mauris. Faucibus scelerisque eleifend donec pretium. Quis lectus nulla at volutpat. 
+        Vel elit scelerisque mauris pellentesque. Nulla facilisi cras fermentum odio eu. 
+        Lacus suspendisse faucibus interdum posuere lorem. Eget aliquet nibh praesent tristique magna sit amet purus. 
+        Non nisi est sit amet facilisis magna etiam tempor. Diam quis enim lobortis scelerisque fermentum dui.")
+post.image.attach(io: File.open(Rails.root.join('frontend/src/assets/img/blog/b2.jpg')), filename: "b2.jpg")
+
+post = Post.create(title: "The Cotton-Jersey Zip-Up Hoodie", content: "Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Neque volutpat ac tincidunt vitae semper quis lectus nulla. Congue mauris rhoncus aenean vel 
+        elit scelerisque mauris. Faucibus scelerisque eleifend donec pretium. Quis lectus nulla at volutpat. 
+        Vel elit scelerisque mauris pellentesque. Nulla facilisi cras fermentum odio eu. 
+        Lacus suspendisse faucibus interdum posuere lorem. Eget aliquet nibh praesent tristique magna sit amet purus. 
+        Non nisi est sit amet facilisis magna etiam tempor. Diam quis enim lobortis scelerisque fermentum dui.")
+post.image.attach(io: File.open(Rails.root.join('frontend/src/assets/img/blog/b1.jpg')), filename: "b1.jpg")
+
+# Products
 product = Product.create(
         name: "Women's Cartoon Astrounaut T-Shirts", 
         desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
@@ -72,5 +107,9 @@ product.images.attach(io: File.open(Rails.root.join('frontend/src/assets/img/pro
 product.images.attach(io: File.open(Rails.root.join('frontend/src/assets/img/products/f4.jpg')), filename: "f4.jpg")
 product.images.attach(io: File.open(Rails.root.join('frontend/src/assets/img/products/f3.jpg')), filename: "f3.jpg")
 product.images.attach(io: File.open(Rails.root.join('frontend/src/assets/img/products/f2.jpg')), filename: "f2.jpg")
+
+# Coupons 
+Coupon.create(code: 'WELCOME10', amount_off: 1000)
+Coupon.create(code: 'WELCOME25', percent_off: 25.5)
 
 
